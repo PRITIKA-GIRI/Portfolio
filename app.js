33a7985c7b7f7e64c889faa5dd095bf40312aa68
Pritika Giri <giri.pri003@gmail.com>
2025-06-08 11:38:47 +0545

    adjust port

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const port = 3000;
-
+const port = process.env.PORT || 3000;
+ 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
